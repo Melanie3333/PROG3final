0d9c84aae62cd90fc97d1779c9c16e2b49e3eae8
Melanie3333 <milli3377mel@gmail.com>
2021-09-29 20:29:03 +0400

    m

--- modules/grass.js
+++ modules/grass.js
@@ -1,26 +1,41 @@
 let LivingCreature = require('./LivingCreature')
 
-module.exports = class Grass extends LivingCreature {
-
-
- 
-    mul() {
-       this.multiply++;
-       var emptyCells = this.chooseCell(0);
-       var newCell = random(emptyCells);
- 
-       console.log(emptyCells);
-       if (newCell && this.multiply >= 8) {
-          var newX = newCell[0];
-          var newY = newCell[1];
-          matrix[newY][newX] = 1;
- 
-          var newGrass = new Grass(newX, newY, this.index);
-          grassArr.push(newGrass);
-          this.multiply = 0;
-       }
+ module.exports = class Grass extends LiveForm {
+        constructor(x, y) {
+            super(x, y);
+            this.multiply = 0;
+        }
+        getNewCoordinates() {
+            this.directions = [
+                [this.x - 1, this.y - 1],
+                [this.x, this.y - 1],
+                [this.x + 1, this.y - 1],
+                [this.x - 1, this.y],
+                [this.x + 1, this.y],
+                [this.x - 1, this.y + 1],
+                [this.x, this.y + 1],
+                [this.x + 1, this.y + 1]
+            ];
+        }
+        chooseCell(character) {
+            this.getNewCoordinates();
+            return super.chooseCell(character);
+        }
+        mul() {
+            this.multiply++;
+            let emptyCells = this.chooseCell(0);
+            let newCell = random(emptyCells);
+    
+            if (newCell && this.multiply >= 2) {
+                grassHashiv++;
+                let x = newCell[0];
+                let y = newCell[1];
+                matrix[y][x] = 1;
+                let grass = new Grass(x, y);
+                grassArr.push(grass);
+                this.multiply = 0;
+            }
+        }
     }
- 
- }
- 
+   
  
